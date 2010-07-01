76ffef85aee9a638c236f48bfb63f1a7031320e5
Andrew Zich <azich@wireless-197-161.uchicago.edu>
2010-07-01 13:52:43 -0500

    Prototype updated to comply with new standards

--- src/main/webapp/ctsasup/templates/main.js
+++ src/main/webapp/ctsasup/templates/main.js
@@ -1,35 +1,167 @@
 if(!window.ctsasup) window.ctsasup = {};
-window.ctsasup.json = function(url,cb) {
-    var id = "callback"+Math.random();
-    var script = document.createElement('script');
-    script.type = "text/javascript";
-    window[id] = function(data) {
-	document.body.removeChild(script);
-	delete window[id];
-	if(cb) cb(data);
+
+/*
+  newBatch() - Create a new Batch
+  .add(uri,callback,data,dataType) - Add work to the Batch
+  .execute() - Do all work in the Batch (in one request if possible)
+*/
+window.ctsasup.newBatch = function() {
+    return {
+	'_todo': [],
+	'_finally': null,
+	'add': function(uri,cb,data,type) {
+	    var me = this;
+	    this._todo.push(function() {//This could look prettier
+		    $.ajax({url:uri,dataType:type,data:data,success:
+			    function(data) {
+				if(cb) cb(data);
+				if(me._todo.length != 0) return;
+				if(me._finally) me._finally();
+			    }
+			})});
+	},
+	'execute': function(cb) {
+	    this._finally = cb;
+	    if(this._todo.length == 0 && cb) cb();
+	    while(this._todo.length > 0) {
+		this._todo.shift().call(this);
+	    }
+	}
     };
-    script.src = url+(url.indexOf("?")<0?"?":"&")+"callback="+escape("window['"+id+"']");
-    document.body.appendChild(script);
 };
-window.ctsasup.fload = function(path,cb) {
-    //Overwrite as needed
-    //this.json("http://localhost/load.php?file="+escape(path),cb);
-    $.get(path,function(data){if(cb)cb(data)});
+
+/*
+  .resolve(base,path) - Basic URI resolver
+*/
+window.ctsasup.resolve = function(base,path) {
+    if(!base || path.match(/^\w+:\/\//)) return path;
+    var results = base.match(/^(.*?)([^\/]*)$/);
+    base = results[1];
+    if(path.match(/^\//)) {
+	var results = base.match(/^(\w+:\/\/[^\/]+)/);
+	if(!results) return path;
+	return results[1]+path;
+    }
+    return base+path;
 };
-window.ctsasup._place = function(id,data) {
+
+/*
+  .get(uri,callback) - Fetch the text content of the requested URI and feed it to callback function
+*/
+window.ctsasup.get = function(uri,cb) {
+    $.get(uri,function(data){if(cb)cb(data)});
+};
+
+/*
+  .place(id,content) - Put the text content into the element with the specified ID
+*/
+window.ctsasup.place = function(id,data) {
     $("#"+id).html(data);
 };
-window.ctsasup.place = function(map) {
+
+/*
+  .contentify(object,[batch]) - Recursively seek out unfilled data requests in object and fill them.
+  If the batch parameter is specified, add them to the batch instead of requesting them directly.
+*/
+window.ctsasup.contentify = function(o,batch) {
+    if(!o || typeof(o) != "object") return;
+    if(o.uri && !o.content) {
+	(batch?batch.add:$.get)(o.uri,function(data) {
+		o.content = data;
+		if(o.callback) o.callback(data);
+	    });
+    }
+    for(var k in o) { this.contentify(o[k]); }
+};
+
+/*
+  .preload(preload,base) - Load/execute a preload object with the specified base URI
+  .preload(preload,base,batch) - Load/execute a preload object with the specified base URI and Batch
+ */
+window.ctsasup.preload = function(preload,base,batch) {
+    if(!preload) return;
     var me = this;
-    for(var id in map) {
-	(function() {
-	    var myid = id;
-	    me.fload(map[myid],function(data){me._place(myid,data)});
+    if(!batch) {
+	var batch = this.newBatch();
+	window.ctsasup.preload(preload,base,batch);
+	batch.execute(); return;
+    }
+    for(var id in preload) {
+	(function() { //Again, scope is too big
+	    var callback = preload[id].callback;
+	    if(callback && typeof(callback) == "string") { callback = eval(callback); }
+	    if(preload[id].content) {
+		if(callback) callback(preload[id].content);
+	    } else if(preload[id].uri) {
+		batch.add(me.resolve(base,preload[id].uri),callback);
+	    }
 	})();
     }
+}
+
+/*
+  .process(descriptor) - Set up the page according to the page descriptor object
+  .process(uri) - Set up the page according to the page descriptor JSON at the specifed URI string
+    base - Use base for relative URIs (optional)
+*/
+window.ctsasup.process = function(desc,base) {
+    if(!desc) return;
+    var me = this;
+    if(typeof(desc) == "string") {
+	$.getJSON(desc,function(data){me.process(data)});
+	return;
+    }
+    if(typeof(desc) != "object") return;
+    var batch = this.newBatch();
+    if(desc.slotMappings) {
+	for(var id in desc.slotMappings) {
+	    if(desc.slotMappings[id].content) {
+		this.processc(id,desc.slotMappings[id].content);
+	    } else if(desc.slotMappings[id].uri) {
+		(function() { //Function wrapper necessary because "id" is overwritten next loop
+		    var myid = id;
+		    var uri = me.resolve(base,desc.slotMappings[myid].uri);
+		    batch.add(uri,function(data) {
+			    desc.slotMappings[myid].content = data; //Why not save it for future use?
+			    me.processc(myid,data,uri);
+			},null,"json");
+		})();
+	    }
+	}
+    }
+    batch.execute(function() {
+	    me.preload(desc.preload,base);
+	});
 };
+
+/*
+  .processc(id,descriptor) - Set up the slot according to the component descriptor object
+  .processc(id,uri) - Set up the slot according to the component descriptor JSON at the specifed URI string
+    base - Use base for relative URIs (optional)
+*/
+window.ctsasup.processc = function(id,desc,base) {
+    if(!desc) return;
+    var me = this;
+    if(typeof(desc) == "string") {
+        $.getJSON(desc,function(data){me.processc(data)});
+        return;
+    }
+    if(typeof(desc) != "object") return;
+    var batch = this.newBatch();
+    if(desc.content.content) {
+	this.place(id,desc.content.content);
+    } else if(desc.content.uri) {
+	batch.add(this.resolve(base,desc.content.uri),function(data) {
+		me.place(id,data);
+	    });
+    }
+    batch.execute(function() {
+	    me.preload(desc.preload,base);
+	});
+};
+
 window.ctsasup._load = function() {
-    if(this.preload) this.preload();
     if(this.load) this.load();
 };
+
 $(document).ready(function(){window.ctsasup._load()});
